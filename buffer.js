0b6f59f76c54dc05fd0a5cfb85cfbccce0e3f8b1
Manh Le <quang.manh@gmail.com>
2017-12-20 22:36:49 -0600

    Global and Buffer

---
+++ buffer.js
@@ -0,0 +1,1 @@
+console.log(Buffer.allocUnsafe(900).fill().toString());
---
+++ process.js
@@ -0,0 +1,12 @@
+process.on('exit',(c)=>{
+    console.log("Exit got called",c);
+})
+
+process.on('uncaughtException',(e)=>{
+    console.log(e);
+    process.exit(1)
+})
+
+process.stdin.resume();
+
+console.dog();
